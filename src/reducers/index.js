9d75672832630249e9ae3664e51c1ddd74133367
ArckWolf <kevinret1@gmail.com>
2018-04-21 09:25:38 +0300

    games work

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -10,13 +10,13 @@ const initialState = {};
 const gameReducer = (state = initialState, action) => {
   switch (action.type) {
     case HANGMAN_GAME_CREATED: {
-      return Object.assign({}, {[action.payload.id]: action.payload});
+      return Object.assign({}, state, {[action.payload.id]: action.payload});
     }
     case RPS_GAME_CREATED: {
       console.log('----RPS_GAME_CREATED----'); // eslint-disable-line no-console
       console.log(state); // eslint-disable-line no-console
       console.log(action); // eslint-disable-line no-console
-      return Object.assign({}, {[action.payload.id]: action.payload});
+      return Object.assign({}, state, {[action.payload.id]: action.payload});
     }
     case HANGMAN_GUESS_SUBMITTED: {
       console.log('----5HANGMAN_GUESS_SUBMITTED5----'); // eslint-disable-line no-console
@@ -24,14 +24,14 @@ const gameReducer = (state = initialState, action) => {
       console.log(state); // eslint-disable-line no-console
       const newGame = state[action.payload.id];
       const newGameState = Object.assign({}, newGame, action.payload);
-      return Object.assign({}, {[newGame.id]: newGameState});
+      return Object.assign({}, state, {[newGame.id]: newGameState});
     }
     case RPS_GUESS_SUBMITTED: {
       console.log('----RPS_GUESS_SUBMITTED----'); // eslint-disable-line no-console
       console.log(state); // eslint-disable-line no-console
       const newGame = state[action.payload.id];
       const newGameState = Object.assign({}, newGame, action.payload);
-      return Object.assign({}, {[newGame.id]: newGameState});
+      return Object.assign({}, state, {[newGame.id]: newGameState});
     }
     default:
       return state;
