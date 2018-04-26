9b7b32ad36337a5b4bb043ac5f61b8c9074138a6
ArckWolf <kevinret1@gmail.com>
2018-04-26 10:03:20 +0300

    Middleware not finished

--- src/middlewares/GameServerMiddleware.js
+++ src/middlewares/GameServerMiddleware.js
@@ -1,31 +1,97 @@
+// Note that the Fetch API is not supported in every browser and may need to be
+// polyfilled in production code:
+// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
+
 import {
-  postNewRpsGameRequest,
- /* postNewRpsGameSucceeded,
+  POST_NEW_RPS_GAME_REQUEST,
+  postNewRpsGameSucceeded,
   postNewRpsGameFaild,
 
+  POST_NEW_HANGMAN_GAME_REQUEST,
+  postNewHangmanGameSucceeded,
+  postNewHangmanGameFaild,
+
+  POST_RPS_GUESS_REQUEST,
   postRpsGuessRequest,
   postRpsGuessSuceeded,
   postRpsGuessFaild,
 
-  postNewHangmanGameRequest,
-  postNewHangmanGameSucceeded,
-  postNewHangmanGameFaild,
-
+  POST_HANGMAN_GUESS_REQUEST,
   postHangmanGuessRequest,
   postHangmanGuessSuceeded,
-  postHangmanGuessFaild*/
-} from '../actions';
+  postHangmanGuessFaild
+} from '../actions/index';
+
+const GUESS_CHAR_TO_GUESS = {
+  'R': 'ROCK',
+  'P': 'PAPER',
+  'S': 'SCISSORS'
+};
 
 const SERVER_ADDRESS = 'http://localhost:8081';
 
-// Define one method which executes the asynchronous process
-export const postNewGame = (fetch = window.fetch) => {
-  console.log('data//////////////.....'); // eslint-disable-line
-  const type = {type: 'rps'};
+export const postHangmanGuess = (id, gameServerID, letter, fetch = window.fetch) => {
   return (dispatch) => {
-    const requestAction = postNewRpsGameRequest({type});
-    dispatch(requestAction);
-    // const localId = requestAction.payload.localId;
+    console.log("postHangmanGuess-----------------------");// eslint-disable-line
+    console.log(id, gameServerID, letter);// eslint-disable-line
+    return fetch(
+      SERVER_ADDRESS + '/games/' + gameServerID + '/moves',
+      {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify({guess: letter})
+      },
+    ).then((response) => {
+       if (response.ok) {
+        response.json().then(
+          (game) => dispatch(postHangmanGuessSuceeded({id, game})),
+          (error) => dispatch(postHangmanGuessFaild({id, error: 'Unparseable response'}))
+        );
+      } else {
+        response.json().then(
+          ({error}) => dispatch(postHangmanGuessFaild({id, error: error})),
+          (error) => dispatch(postHangmanGuessFaild({id, error: 'Unparseable response'}))
+        );
+      }
+    }).catch((error) => {
+      dispatch(dispatch(postHangmanGuessFaild({id, error: 'Service unreachable'})));
+    });
+  };
+};
+
+export const postRpsGuess = (id, gameServerID, letter, fetch = window.fetch) => {
+  return (dispatch) => {
+    return fetch(
+      SERVER_ADDRESS + '/games/' + gameServerID + '/moves',
+      {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify({guess: GUESS_CHAR_TO_GUESS[letter]})
+      },
+    ).then((response) => {
+       if (response.ok) {
+        response.json().then(
+          (game) => dispatch(postRpsGuessSuceeded({id, game})),
+          (error) => dispatch(postRpsGuessFaild({id, error: 'Unparseable response'}))
+        );
+      } else {
+        response.json().then(
+          ({error}) => dispatch(postRpsGuessFaild({id, error: error})),
+          (error) => dispatch(postRpsGuessFaild({id, error: 'Unparseable response'}))
+        );
+      }
+    }).catch((error) => {
+      dispatch(dispatch(postRpsGuessFaild({id, error: 'Service unreachable'})));
+    });
+  };
+};
+
+export const postNewRpswGame = (id, fetch = window.fetch) => {
+  return (dispatch) => {
     return fetch(
       SERVER_ADDRESS + '/games/',
       {
@@ -33,39 +99,72 @@ export const postNewGame = (fetch = window.fetch) => {
         headers: {
           'content-type': 'application/json'
         },
-        body: JSON.stringify({type})
+        body: JSON.stringify({type: 'rps'})
       },
     ).then((response) => {
-      console.log('responce//////////////.....'); // eslint-disable-line
-      console.log(response); // eslint-disable-line
-      /* if (response.ok) {
+       if (response.ok) {
         response.json().then(
-          ({id}) => dispatch(postCommentSucceeded({localId, id})),
-          (error) => dispatch(postCommentFailed({localId, error: 'Unparseable response'}))
+          (game) => dispatch(postNewRpsGameSucceeded({id, game})),
+          (error) => dispatch(postNewRpsGameFaild({id, error: 'Unparseable response'}))
         );
       } else {
         response.json().then(
-          ({error}) => dispatch(postCommentFailed({localId, error: error})),
-          (error) => dispatch(postCommentFailed({localId, error: 'Unparseable response'}))
+          ({error}) => dispatch(postNewRpsGameFaild({id, error: error})),
+          (error) => dispatch(postNewRpsGameFaild({id, error: 'Unparseable response'}))
         );
-      }*/
+      }
     }).catch((error) => {
-      console.log('error//////////////.....'); // eslint-disable-line
-      console.log(error); // eslint-disable-line
-      /* dispatch(dispatch(postCommentFailed({localId, error: 'Service unreachable'})));*/
+      dispatch(dispatch(postNewRpsGameFaild({id, error: 'Service unreachable'})));
     });
   };
 };
 
+export const postNewHangmanwGame = (id, fetch = window.fetch) => {
+  return (dispatch) => {
+    return fetch(
+      SERVER_ADDRESS + '/games/',
+      {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify({type: 'hangman'})
+      },
+    ).then((response) => {
+       if (response.ok) {
+        response.json().then(
+          (game) => dispatch(postNewHangmanGameSucceeded({id, game})),
+          (error) => dispatch(postNewHangmanGameFaild({id, error: 'Unparseable response'}))
+        );
+      } else {
+        response.json().then(
+          ({error}) => dispatch(postNewHangmanGameFaild({id, error: error})),
+          (error) => dispatch(postNewHangmanGameFaild({id, error: 'Unparseable response'}))
+        );
+      }
+    }).catch((error) => {
+      dispatch(dispatch(postNewHangmanGameFaild({id, error: 'Service unreachable'})));
+    });
+  };
+};
 
-const commentServerMiddleware = (store) => (next) => {
+const gameServerMiddleware = (store) => (next) => {
   return (action) => {
-    console.log('commentServerMiddleware//////////////.....'); // eslint-disable-line
-    console.log(action); // eslint-disable-line
-
-    store.dispatch(postNewGame());
+    console.log("gameServerMiddleware-----------------------");// eslint-disable-line
+    console.log(action);// eslint-disable-line
+    if (action.type === POST_NEW_HANGMAN_GAME_REQUEST) {
+      const id = action.payload.id;
+      store.dispatch(postNewHangmanwGame(id));
+    } else if (action.type === POST_NEW_RPS_GAME_REQUEST) {
+      const id = action.payload.id;
+      store.dispatch(postNewRpswGame(id));
+    } else if (action.type === POST_RPS_GUESS_REQUEST) {
+      store.dispatch(postRpsGuess(action.payload.id, action.payload.gameServerId));
+    } else if (action.type === POST_HANGMAN_GUESS_REQUEST) {
+      store.dispatch(postHangmanGuess(action.payload.id, action.payload.gameServerId));
+    }
     return next(action);
   };
 };
 
-export default commentServerMiddleware;
+export default gameServerMiddleware;
